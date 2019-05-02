a47245663878e830719398ac7ad9ef6346a7c9d6
saijal <saijalshakya@gmail.com>
2019-05-02 22:29:24 +0545

    feat: user can register

---
+++ cosmo_user/common/register_user.py
@@ -0,0 +1,25 @@
+from cosmo_user import models as cosmo_models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+
+def register_django_user(request):
+    try:
+        user = User.objects.create(username=request.POST['username'],
+                                   email=request.POST['email'], first_name=request.POST['first_name'],
+                                   last_name=request.POST['last_name'])
+        user.set_password(request.POST['password'])
+        user.save()
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+def register_cosmo_user(user):
+        try:
+            cosmo_models.CosmoUser.objects.create(user=user, votingCount=25,verified=False,token=2580,expiry=datetime.now())
+            return True
+        except Exception as e:
+            print(e)
+            return False
--- cosmo_user/views.py
+++ cosmo_user/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from cosmo_user import models as cosmo_models
 from django.contrib import messages
-
+from cosmo_user.common import register_user
 
 def user_login(request):
     if request.method == 'GET':
@@ -39,58 +39,36 @@ def user_logout(request):
 
 
 def user_register(request):
-    context = {
-        'departments': Department.objects.all()
-    }
-    leaves = get_all_leaves_unseen()
-    context.update({'leave_notify':leaves})
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('user-login'))
-    if not is_leave_issuer(request.user):
-        return HttpResponseRedirect(reverse('leave_manager_dashboard'))
-    routes = get_formatted_routes(get_routes(request.user), active_page='register user')
-    context.update({'routes': routes})
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('user-index'))
     if request.method == 'GET':
-        if not request.user.is_authenticated:
-            url = reverse('user-login')
-            return HttpResponseRedirect(url)
-        return render(request, 'lms_user/register.html', context=context)
+        return render(request, 'cosmo_user/register.html')
 
     if request.method == 'POST':
-        if not request.user.is_authenticated:
-            url = reverse('user-login')
-            return HttpResponseRedirect(url)
-        else:
-            try:
-                if register_django_user(request):
-                    user = User.objects.get(username=request.POST['username'])
-                    department = Department.objects.get(id=int(request.POST['department']))
-                    lms_user_details = {
-                        'user': user,
-                        'phone_number': request.POST['phone_number'],
-                        'department': department,
-                        'leave_issuer': department.head_of_department,
-                        'date_of_birth': datetime.strptime(request.POST['date_of_birth'], '%Y-%m-%d'),
-                        'joined_date': datetime.strptime(request.POST['joined_date'], '%Y-%m-%d')
-                    }
-                    if register_lms_user(user_details=lms_user_details):
-                        return HttpResponseRedirect(reverse('user-index'))
-                    else:
-                        try:
-                            user.delete()
-                            context.update({'message': 'Could register to LMS. Please contact Admin or try again later'})
-                            return render(request, 'lms_user/register.html', context=context)
-                        except Exception as e:
-                            print(e)
-                            return render(request, 'lms_user/register.html', context=context)
+        try:
+            if register_user.register_django_user(request):
+                user = User.objects.get(username=request.POST['username'])
+                
+                if register_user.register_cosmo_user(user=user):
+                    messages.success(request, "You have been successfully register.", extra_tags="1")
+                    return HttpResponseRedirect(reverse('user-index'))
                 else:
-                    context.update({'message': 'Could register to Django. Please contact Admin or try again later'})
-                    return render(request, 'lms_user/register.html', context=context)
+                    try:
+                        user.delete()
+                        messages.success(request, "Could not register. Please try again.", extra_tags="1")
+                        return HttpResponseRedirect(reverse('user-register'))
+                    except Exception as e:
+                        print(e)
+                        return render(request, 'cosmo_user/register.html')
+            else:
+                messages.success(request, "Could not register. Please try again.", extra_tags="1")
+                return HttpResponseRedirect(reverse('user-register'))
 
-            except Exception as e:
-                print(e)
-                context.update({'message': 'Could not register. Please contact Admin or try again later'})
-                return render(request, 'lms_user/register.html', context=context)
+        except Exception as e:
+            print(e)
+            messages.success(request, "Could not register. Please try again.", extra_tags="1")
+            return HttpResponseRedirect(reverse('user-register'))
+            
 
 
 def index(request):
