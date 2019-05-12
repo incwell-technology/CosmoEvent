08f4499c523143d76e33225522635f4bbcf1ecac
saijal <saijalshakya@gmail.com>
2019-05-12 15:03:49 +0545

    feat: yaml for settings

--- CosmoEvent/settings.py
+++ CosmoEvent/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import yaml
 
+credentials = yaml.load(open('credentials.yaml'), Loader=yaml.FullLoader)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = credentials['app_secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
