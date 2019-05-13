e75e6a16c4be15bbf180a9663e95ce996baa1e2c
saijal <saijalshakya@gmail.com>
2019-05-13 20:32:47 +0545

    bug: email exception handled

--- cosmo_user/common/resend_verification_email.py
+++ cosmo_user/common/resend_verification_email.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from datetime import datetime
 import socket
-
+import urllib
 
 def resend_verification_email(update_details, cosmo_user, verification_code):
 
@@ -22,17 +22,22 @@ def resend_verification_email(update_details, cosmo_user, verification_code):
         msg['To'] = recipient
 
         try:
-            cosmo_user.token = verification_code
-            cosmo_user.resend_code = False
-            cosmo_user.expiry = datetime.now()
-            cosmo_user.save()
+            try:
+                response=urllib.request.urlopen('https://www.google.com',timeout=1)
+                cosmo_user.token = verification_code
+                cosmo_user.resend_code = False
+                cosmo_user.expiry = datetime.now()
+                cosmo_user.save()
 
-            server = smtplib.SMTP_SSL(credentials['smtp_server'], credentials['smtp_port'])
-            server.login(sender, password)
-            server.sendmail(sender, [recipient], msg.as_string())
-            server.quit()
-            return True
-        except smtplib.SMTPException:
+                server = smtplib.SMTP_SSL(credentials['smtp_server'], credentials['smtp_port'])
+                server.login(sender, password)
+                server.sendmail(sender, [recipient], msg.as_string())
+                server.quit()
+                return True
+            except Exception as err:
+                print(err)
+                return False
+        except smtplib.SMTPException as e:
             print(e)
             return False
     except socket.gaierror:
